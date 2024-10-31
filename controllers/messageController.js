58ac9ff74981de5a27bdd89d4e81df2d5bbae0a4
SamuelJeff <samueljefferson143@gmail.com>
2024-10-31 14:52:58 -0300

    inclusão de detalhemento de mensagens e algumas organizações estrutura de código

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,108 +1,100 @@
 // controllers/messageController.js
 
-let userSessions = {};
+var userSessions = {};
+var optionRegister = 0
 
-function sendWelcomeMessage(client, from) {
-    const welcomeMessage = 
-        "Bem-vindo ao Suporte de TI!\n\n" +
-        "Estamos aqui para ajudá-lo com seus chamados de suporte técnico.\n";
-    client.sendMessage(from, welcomeMessage);
+async function sendWelcomeMessage(client, from) {
+    const welcomeMessage = "Bem-vindo ao Suporte de TI!\n\n";
+    await client.sendMessage(from, welcomeMessage);
 }
 
-function askForName(client, from) {
-    client.sendMessage(from, "Para começarmos, por favor, informe o seu nome.");
-    userSessions[from] = 'awaiting_name';
+async function askForName(client, from) {
+    const nameMessage = "Para começar, qual o seu nome?";
+    await client.sendMessage(from, nameMessage);
 }
 
-function sendWelcomeMenu(client, from) {
-    const welcomeMenu = 
+async function askForAgency(client, from) {
+    const agencyMessage = "Por favor, informe a unidade/setor à qual você pertence.";
+    await client.sendMessage(from, agencyMessage);
+}
+
+async function sendWelcomeMenu(client, from) {
+    const welcomeMenu =
         "Como podemos ajudá-lo hoje?\n\n" +
         "1️⃣ Abrir chamado para Suporte\n" +
         "2️⃣ Troca de senha\n" +
         "3️⃣ Manutenção\n" +
         "4️⃣ Instalação ou Atualização de Software\n\n" +
         "Por favor, responda com o número da opção desejada.";
-    client.sendMessage(from, welcomeMenu);
-    userSessions[from] = 'awaiting_selection';
+    await client.sendMessage(from, welcomeMenu);
 }
 
-function handleOptionSelection(client, from, option) {
+async function handleOptionSelection(client, from, option) {
     switch (option) {
         case '1':
-            client.sendMessage(from, "Você escolheu: Suporte. Descreva o problema que você está enfrentando.");
-            userSessions[from] = 'suporte';
+            await client.sendMessage(from, "Você escolheu: Suporte. Por favor, descreva em detalhes o problema que você está enfrentando.");
+            userSessions[from] = 'awaiting_detailed_description_support';
+            optionRegister = "Suporte"
             break;
         case '2':
-            client.sendMessage(from, "Você escolheu: Troca de senha. Qual serviço necessita da troca de senha (ex: e-mail, sistema interno, etc)?");
-            userSessions[from] = 'troca_senha';
+            await client.sendMessage(from, "Você escolheu: Troca de senha. Qual serviço necessita da troca de senha (ex: e-mail, sistema interno, etc)?");
+            userSessions[from] = 'awaiting_detailed_description_password';
+            optionRegister = "Troca de senha"
             break;
         case '3':
-            client.sendMessage(from, "Você escolheu: Manutenção. Que tipo de manutenção é necessária (ex: hardware, software)?");
-            userSessions[from] = 'manutencao';
+            await client.sendMessage(from, "Você escolheu: Manutenção. Que tipo de manutenção é necessária (ex: hardware, software)?");
+            userSessions[from] = 'awaiting_maintenance_type';
+            optionRegister = "Manutenção"
             break;
         case '4':
-            client.sendMessage(from, "Você escolheu: Instalação ou Atualização de Software. Por favor, informe o software a ser instalado ou atualizado.");
-            userSessions[from] = 'instalacao_software';
+            await client.sendMessage(from, "Você escolheu: Instalação ou Atualização de Software. Por favor, informe o software a ser instalado ou atualizado.");
+            userSessions[from] = 'awaiting_detailed_description_installation';
+            optionRegister = "Instalação ou Atualização de Software"
             break;
         default:
-            client.sendMessage(from, "Opção inválida. Por favor, responda com o número correspondente.");
-            sendWelcomeMenu(client, from);
+            await client.sendMessage(from, "Opção inválida. Por favor, responda com o número correspondente.");
+            await sendWelcomeMenu(client, from);
             break;
     }
 }
 
-function askForAgency(client, from) {
-    client.sendMessage(from, "Por favor, informe a sua unidade.");
-    userSessions[from] = 'awaiting_agency';
-}
-
-function handleMessage(client, message) {
+async function handleUserMessage(client, message) {
     const from = message.from;
     const userMessage = message.body.trim();
 
+    // Verifica o estado atual do usuário e realiza a ação correspondente
     if (!userSessions[from]) {
-        sendWelcomeMessage(client, from);
-        setTimeout(() => {
-            askForName(client, from);
-        }, 1000);
+        userSessions[from] = 'awaiting_name';
+        await sendWelcomeMessage(client, from);
+        await askForName(client, from);
     } else {
         const currentState = userSessions[from];
 
         if (currentState === 'awaiting_name') {
-            client.sendMessage(from, `Prazer em conhecê-lo, ${userMessage}!`);
-            userSessions[from] = { name: userMessage };
-            askForAgency(client, from);
+            await client.sendMessage(from, `Prazer em conhecê-lo, ${userMessage}!`);
+            userSessions[from] = 'awaiting_agency';
+            await askForAgency(client, from);
         } else if (currentState === 'awaiting_agency') {
-            client.sendMessage(from, `Agência registrada: ${userMessage}.`);
-            userSessions[from].agency = userMessage;
-            setTimeout(() => {
-                sendWelcomeMenu(client, from);
-            }, 1000);
+            await client.sendMessage(from, `Agência registrada: ${userMessage}.`);
+            userSessions[from] = 'awaiting_selection';
+            await sendWelcomeMenu(client, from);
         } else if (currentState === 'awaiting_selection') {
-            handleOptionSelection(client, from, userMessage);
-        } else if (currentState === 'suporte') {
-            client.sendMessage(from, `Chamado de Suporte registrado: "${userMessage}". Nossa equipe entrará em contato em breve.`);
-            delete userSessions[from];
-        } else if (currentState === 'troca_senha') {
-            client.sendMessage(from, `Pedido de troca de senha para o serviço "${userMessage}" registrado. Nossa equipe vai processar sua solicitação.`);
-            delete userSessions[from];
-        } else if (currentState === 'manutencao') {
-            client.sendMessage(from, `Pedido de manutenção registrado para "${userMessage}". Nossa equipe está a caminho.`);
-            delete userSessions[from];
-        } else if (currentState === 'instalacao_software') {
-            client.sendMessage(from, `Instalação/atualização do software "${userMessage}" registrada. Vamos providenciar o necessário.`);
-            delete userSessions[from];
+            await handleOptionSelection(client, from, userMessage);
+        } else if (currentState === 'awaiting_maintenance_type') {
+            await client.sendMessage(from, "Por favor, descreva em uma mensagem qual o problema a ser solucionado.");
+            userSessions[from] = 'awaiting_detailed_description_maintenance';
+
+        } else if (currentState.startsWith('awaiting_detailed_description')) {
+            await client.sendMessage(from, `Chamado registrado: "${optionRegister}". Nossa equipe entrará em contato em breve.`);
+            delete userSessions[from]; // Reseta o estado do usuário após o chamado ser registrado
         }
     }
 }
 
-
+async function Savemessage(params) {
+    
+}
 
 module.exports = {
-    sendWelcomeMessage,
-    askForName,
-    sendWelcomeMenu,
-    handleOptionSelection,
-    askForAgency,
-    handleMessage,
+    handleUserMessage
 };
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
+// main.js
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const { handleSaveSession, handleLoadSession, handleDeleteSession } = require('./controllers/sessionController');
-const { handleMessage } = require('./controllers/messageController');
+const { handleUserMessage } = require('./controllers/messageController');
 
-// Configuração do cliente do WhatsApp com LocalAuth
 const client = new Client({
     authStrategy: new LocalAuth({
         clientId: "suporte_ti_bot",
@@ -16,25 +16,19 @@ const client = new Client({
     })
 });
 
-// Evento para exibir o QR Code
 client.on('qr', (qr) => {
     console.log('QR code recebido, escaneie com o WhatsApp:');
     qrcode.generate(qr, { small: true });
 });
 
-// Evento para confirmar que o cliente está pronto
 client.on('ready', () => {
     console.log('Cliente pronto!');
 });
 
-// Evento para lidar com mensagens
-client.on('message', async (message) => {
-    await handleMessage(client, message);
-    
-console.log(message.body)    
+client.on('message', (message) => {handleUserMessage(client, message)
+    console.log(message.body)
 });
 
 
 
-// Iniciar o cliente
 client.initialize();
