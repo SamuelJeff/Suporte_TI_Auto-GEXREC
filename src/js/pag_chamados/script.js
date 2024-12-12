6f52f12f565a9de65c3bf62ef8b734f2752199e0
Vitoria Regina <regina117.500@hotmail.com>
2024-12-12 03:28:38 -0300

    Quase projeto

--- src/js/pag_chamados/script.js
+++ src/js/pag_chamados/script.js
@@ -3,8 +3,50 @@ document.addEventListener("DOMContentLoaded", () => {
     const searchInput = document.getElementById("search");
 
     const chamados = [
-        { numero: 1, solicitante: "João Silva", setor: "TI", categoria: "Erro de sistema", descricao: "Erro ao acessar o portal", data: "12/12/2024", estado: "Aberto" },
-        { numero: 2, solicitante: "Maria Oliveira", setor: "RH", categoria: "Dúvida", descricao: "Consulta sobre benefícios", data: "10/12/2024", estado: "Encerrado" },
+        { numero: 1, 
+            solicitante: "João Silva", 
+            setor: "APS - Areias", 
+            tipo: "Abrir chamado para suporte", 
+            categoria: "Erro de sistema", 
+            descricao: "Erro ao acessar o portal", 
+            data: "04/12/2024", 
+            estado: "Encerrado" },
+
+        { numero: 2, 
+            solicitante: "Juan José", 
+            setor: "APS São Lourenço da Mata", 
+            tipo: "Manutenção", 
+            categoria: "Hardware", 
+            descricao: "Meu computador quebrou", 
+            data: "06/12/2024", 
+            estado: "Encerrado" },
+
+        { numero: 3, 
+            solicitante: "Maria Oliveira", 
+            setor: "APS Cabo", 
+            tipo: "Troca de Senha", 
+            categoria: "GERID", 
+            descricao: "preciso acessar o PAT", 
+            data: "09/12/2024", 
+            estado: "Encerrado" },
+
+        { numero: 4, 
+            solicitante: "Fábio Rodrigues", 
+            setor: "APS Pina", 
+            tipo: "Instalação ou Atualização de Software", 
+            categoria: "VPN", 
+            descricao: "VPN para acessar o SABI", 
+            data: "10/12/2024", 
+            estado: "Encerrado" },
+
+        { numero: 5, 
+            solicitante: "Samuel Jefferson", 
+            setor: "GEXREC 4º andar", 
+            tipo: "Manutenção", 
+            categoria: "Hardware", 
+            descricao: "Meu computador não liga", 
+            data: "11/12/2024", 
+            estado: "Aberto" }
     ];
 
     const renderTable = (filtro = "") => {
@@ -17,6 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     <td>${chamado.numero}</td>
                     <td>${chamado.solicitante}</td>
                     <td>${chamado.setor}</td>
+                    <td>${chamado.tipo}</td>
                     <td>${chamado.categoria}</td>
                     <td>${chamado.descricao}</td>
                     <td>${chamado.data}</td>
